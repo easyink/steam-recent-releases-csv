2f015281fd83a3a5d1116c30fc25a73f7a111479
Dylan L <43196885+easyink@users.noreply.github.com>
2020-06-20 12:53:37 -0400

    Update script_v4.py
    Fixed the discounted price bug and also split it into two columns on the csv. also compacted the Free game detection thanks to a german

--- script_v4.py
+++ script_v4.py
@@ -10,7 +10,7 @@ pages_to_search = 8
 def generate_csv(csvname, base_url, numofpages):
     with open(csvname, 'w', encoding='utf-8', newline='') as csvfile:
         writecsv = csv.writer(csvfile, delimiter=',')
-        writecsv.writerow(['Name of Game', 'Release Date', 'Price', 'Link To Game']) #base row 
+        writecsv.writerow(['Name of Game', 'Release Date', 'Current Price', 'Original Price', 'Discount Percentage', 'Link To Game']) #base row 
         for num in range( 1 , numofpages+1 ):
             url = base_url + str(num)
             print('Went To ', url)
@@ -24,12 +24,17 @@ def generate_csv(csvname, base_url, numofpages):
                 release_date = i.find('div', class_='col search_released responsive_secondrow').get_text()
                 if i.find('div', class_= 'col search_price discounted responsive_secondrow'): #if game is discounted
                     discounted = i.find('div', class_= 'col search_price discounted responsive_secondrow')
-                    game_price = discounted.get_text(strip=True) #unfortunately currently gets both original price and discounted price in a joined string. idk how to fix
+                    game_price_list = discounted.get_text(strip=True).split("$") #splits the price string in two
+                    game_price = game_price_list[1] #first one is the discounted current price
+                    og_price = game_price_list[2] #second one is the original price
+                    discount_percentage = i.find('div', class_='col search_discount responsive_secondrow').get_text(strip=True) #gets the percentage off
                 else:
                     game_price = i.find('div', class_='col search_price responsive_secondrow').get_text(strip=True) 
-                if len(game_price) == 0 or game_price == 'Free' or game_price == 'Free To Play' or game_price == 'Free to Play': #skips entry if the game is free or not out yet(doesnt have a price)
-                    continue
-                writecsv.writerow([ game_title , release_date , game_price, url_link ]) #write to csv!!
+                    og_price = ''
+                    discount_percentage = ''
+
+                if game_price and 'Free' not in game_price:
+                    writecsv.writerow([ game_title , release_date , game_price, og_price, discount_percentage, url_link ])
     print('Generated', csvname)
 
 generate_csv(namecsv,url_filter,pages_to_search)
